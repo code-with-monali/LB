b1196b8aded32661958cee99e8870a21dfc11a5c
Monali Ranjun <165270530+code-with-monali@users.noreply.github.com>
2024-03-28 09:31:38 -0700

    Add files via upload

---
+++ program2.c
@@ -0,0 +1,16 @@
+//Addition of two numbers
+//ha static vala pgm aahe bcoz agodar ch input mahitey. lihtanach input thrvave lagte
+// static input application(compile time)
+#include<stdio.h>
+
+int main()
+{
+    int No1 = 10;
+    int No2 = 11;
+    int Ans = 0;
+
+    Ans = No1+No2;  // yethe operation jhale
+
+    printf("%d\n", Ans);
+    return 0;
+}
