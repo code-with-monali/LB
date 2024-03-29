f8d90421d71e04b245e0345a01aad2a86988b072
Monali Ranjun <165270530+code-with-monali@users.noreply.github.com>
2024-03-29 00:45:05 -0700

    Add files via upload

---
+++ program337.java
@@ -0,0 +1,34 @@
+//Accept number from user and return multiplication of even numbers factor
+import java.util.*;
+
+class Digits
+{
+    int CountDigits(int iValue)
+    {
+       int iCount = 0;
+
+       while(iValue != 0)
+       {
+        iCount++;
+        iValue = iValue / 10;
+       }
+       return iCount;
+    }
+
+}
+
+class program337
+{
+    public static void main (String arg[])
+    {
+       Scanner sobj = new Scanner(System.in);
+
+       System.out.println("Enter number :");
+       int iNo = sobj.nextInt();
+        
+       Digits dobj = new Digits();
+       int iRet = dobj.CountDigits(iNo);
+       System.out.println("Number of digits are : "+iRet); 
+ 
+    }
+}
---
+++ program338.java
@@ -0,0 +1,36 @@
+//Accept number from user and return multiplication of even numbers factor
+import java.util.*;
+
+class Digits
+{
+    int SumDigits(int iValue)
+    {
+       int iSum = 0;
+       int iDigit = 0;
+
+       while(iValue != 0)
+       {
+        iDigit = iValue % 10;
+        iSum = iSum + iDigit;
+        iValue = iValue / 10;
+       }
+       return iSum;
+    }
+
+}
+
+class program338
+{
+    public static void main (String arg[])
+    {
+       Scanner sobj = new Scanner(System.in);
+
+       System.out.println("Enter number :");
+       int iNo = sobj.nextInt();
+        
+       Digits dobj = new Digits();
+       int iRet = dobj.SumDigits(iNo);
+       System.out.println("Number of digits are : "+iRet); 
+ 
+    }
+}
